bb07e016950df0589d618b1b4944654c0e24b0b9
decaster3 <rinat777hatipov@gmail.com>
2017-09-25 16:28:55 +0300

    fix bugs

--- Siri/ViewController.swift
+++ Siri/ViewController.swift
@@ -2,8 +2,8 @@
 //  ViewController.swift
 //  Siri
 //
-//  Created by Sahand Edrisian on 7/14/16.
-//  Copyright © 2016 Sahand Edrisian. All rights reserved.
+//  Created by Rinat Khatipov.
+//  
 //
 
 import UIKit
